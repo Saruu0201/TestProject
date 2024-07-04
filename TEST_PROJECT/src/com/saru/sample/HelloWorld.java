2083061f7a46e75a2ebdf085cc0f0e3927e40efb
Saruu0201 <sarawathina333@gmail.com>
2024-07-04 22:34:15 +0530

    FIRST TEST PROJECT

---
+++ TEST_PROJECT/src/com/saru/sample/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.saru.sample;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("WELCOME BACK SARU");
+	}
+
+}
